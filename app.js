1378eaca6da21eb1854e3ca14ff10155fb796b01
eileen <zwleileen@gmail.com>
2024-12-16 13:28:50 +0800

    feat: assigned numbers to buttons and display

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const numberButtons = document.querySelectorAll(".button.number");
 
-numberButtons.forEach((numberbutton, index) => {
+numberButtons.forEach((numberButton, index) => {
   const number = index + 1;
-  numberbutton.textContent = number; //note: sets visible text in number button
-  numberbutton.dataset.number = number; //note: sets a data-number attribute in HTML of the number button
+  numberButton.textContent = number; //note: sets visible text in number button
+  numberButton.dataset.number = number; //note: sets a data-number attribute in HTML of the number button
 });
 
-const secondButton = numberButtons[1];
-console.log(secondButton.textContent);
+// const secondButton = numberButtons[1];
+// console.log(secondButton.textContent);
+
+const display = document.querySelector(".display");
+numberButtons.forEach((numberButton) => {
+  numberButton.addEventListener("click", () => {
+    display.textContent = numberButton.textContent;
+    console.log(display.textContent);
+  });
+});
