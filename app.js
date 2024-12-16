6948ce0a1ff33646993de67ae493747557c9bded
eileen <zwleileen@gmail.com>
2024-12-16 13:51:20 +0800

    feat:reassigned text content to number button

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const numberButtons = document.querySelectorAll(".button.number");
 
-numberButtons.forEach((numberButton, index) => {
-  const number = index + 1;
-  numberButton.textContent = number; //note: sets visible text in number button
-  numberButton.dataset.number = number; //note: sets a data-number attribute in HTML of the number button
-});
+// numberButtons.forEach((numberButton) => {
+//   //   numberButton.textContent = number; //note: sets visible text in number button
+//   //   numberButton.dataset.number = number; //note: sets a data-number attribute in HTML of the number button
+// });
 
 // const secondButton = numberButtons[1];
 // console.log(secondButton.textContent);
@@ -13,6 +12,5 @@ const display = document.querySelector(".display");
 numberButtons.forEach((numberButton) => {
   numberButton.addEventListener("click", () => {
     display.textContent = numberButton.textContent;
-    console.log(display.textContent);
   });
 });
