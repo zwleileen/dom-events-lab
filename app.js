2402a728fee8f108364688ba8f489085a1a08ea6
eileen <zwleileen@gmail.com>
2024-12-16 14:16:04 +0800

    feat:try summing

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const numberButtons = document.querySelectorAll(".button.number");
+const buttons = document.querySelectorAll(".button");
 
 // numberButtons.forEach((numberButton) => {
 //   //   numberButton.textContent = number; //note: sets visible text in number button
@@ -9,8 +9,21 @@ const numberButtons = document.querySelectorAll(".button.number");
 // console.log(secondButton.textContent);
 
 const display = document.querySelector(".display");
-numberButtons.forEach((numberButton) => {
-  numberButton.addEventListener("click", () => {
-    display.textContent = numberButton.textContent;
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.textContent === "C") {
+      display.textContent = "0";
+    } else {
+      display.textContent = button.textContent;
+    }
   });
+});
+
+const numberButtons = document.querySelectorAll(".button.number");
+const operatorButtons = document.querySelectorAll("button.operator");
+
+let sum = 0;
+// console.log(numberButtons[1].textContent);
+numberButtons.addEventListener("click", () => {
+  sum += Number(numberButtons); //convert button's textcontent to a number
 });
